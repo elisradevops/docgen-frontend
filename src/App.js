4ff93f9fcc89fec76a40e747c8c2f8b2dd39dc6d
Eden Zvi Schwartz <eden@korentec.co.il>
2024-07-18 15:08:14 +0300

    app prettier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from "react";
-import MainTabs from "./components/tabs/MainTabs";
-import SettingsPage from "./components/settings/SettingPage";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { useCookies } from "react-cookie";
+import React from 'react';
+import MainTabs from './components/tabs/MainTabs';
+import SettingsPage from './components/settings/SettingPage';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useCookies } from 'react-cookie';
 
 function App({ store }) {
-  const [cookies, setCookie] = useCookies(["azuredevopsUrl", "azuredevopsPat"]);
+  const [cookies, setCookie] = useCookies(['azuredevopsUrl', 'azuredevopsPat']);
 
   const login = (selectedUrl, selectedPat) => {
     let d = new Date();
-    d.setTime(d.getTime() + (525960*60*1000));
-    setCookie("azuredevopsUrl", selectedUrl, { path: '/' , expires: d});
-    setCookie("azuredevopsPat", selectedPat, { path: '/' , expires: d});
+    d.setTime(d.getTime() + 525960 * 60 * 1000);
+    setCookie('azuredevopsUrl', selectedUrl, { path: '/', expires: d });
+    setCookie('azuredevopsPat', selectedPat, { path: '/', expires: d });
     window.location.reload(); // Reload the page after signing in
-
   };
 
   return (
-    <div className="App">
+    <div className='App'>
       <ToastContainer />
       {cookies.azuredevopsUrl && cookies.azuredevopsPat ? (
         <MainTabs store={store} />
