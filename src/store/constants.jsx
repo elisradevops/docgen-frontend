da4b2cab1108fca4db91c46d51f19d7f888b99e7
Eden Zvi Schwartz <eden@korentec.co.il>
2026-02-01 15:23:09 +0200

    #269641 Embed Docgen into ADO
    - Edit the constants to be dynamic per build

--- src/store/constants.jsx
+++ src/store/constants.jsx
@@ -1,9 +1,21 @@
-const buildTimeUrl = (import.meta.env && import.meta.env.VITE_DOCGEN_API_URL) || '';
-const jsonDocument_url =
-  buildTimeUrl ||
-  (window.location.hostname !== 'localhost'
-    ? `http://${window.location.hostname}:30001`
-    : 'http://localhost:30001');
+// const buildTimeUrl = (import.meta.env && import.meta.env.VITE_DOCGEN_API_URL) || '';
+// const jsonDocument_url =
+//   buildTimeUrl ||
+//   (window.location.hostname !== 'localhost'
+//     ? `http://${window.location.hostname}:30001`
+//     : 'http://localhost:30001');
+
+// let C = {
+//   jsonDocument_url,
+// };
+
+// export default C;
+
+const jsonDocument_url = window.APP_CONFIG?.JSON_DOCUMENT_URL ?? '';
+
+if (!jsonDocument_url) {
+  throw new Error('JSON_DOCUMENT_URL is not defined in APP_CONFIG');
+}
 
 let C = {
   jsonDocument_url,
